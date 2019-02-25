839d6ba03abaf92e767af9a9864b7baa5eafc79a
aerain <j40f893@gmail.com>
2019-02-26 01:05:08 +0900

    test

--- libs/lolapi.js
+++ libs/lolapi.js
@@ -51,38 +51,38 @@ class LeagueOfLegendsAPI {
         return body.accountId;
     }
 
-    async getMatchListFromAccountId(accountId, page=5, queue = null) {
+    async getMatchListFromAccountId(accountId, beginIndex=0, queue = null) {
         if(!this.hasAppkey()) return;
         let uri = `https://kr.api.riotgames.com/lol/match/v4/matchlists/by-account/${accountId}?api_key=${this.app_key}${queue !== null ? `&queue=${queue}` : ''}`
         let data = [];
-        // let beginIndex = 0;
-        // let lastIndex = 0;
-        // let response = await fetch(`${uri}&beginIndex=${beginIndex}&endIndex=${}`);
-        // let body = await response.json();
+        
+        let response = await fetch(`${uri}&beginIndex=${beginIndex}`);
+        let body = await response.json();
 
-        // if(!this.verifyBody(body)) return;
+        if(!this.verifyBody(body)) return;
 
-        // var { matches } = body;
-        // matches.map(({ gameId, champion }) => data.push({ gameId, champion }));
-        let dataSize = 100;
-        let index = 0;
-        let totalGames = 0;
+        var { matches } = body;
+        matches.map(({ gameId, champion }) => data.push({ gameId, champion }));
+        
+        // let dataSize = 100;
+        // let index = 0;
+        // let totalGames = 0;
 
-        do {        
-            let response = await fetch(`${uri}&beginIndex=${index}&endIndex=${index+dataSize}`);
-            let body = await response.json();
+        // do {        
+        //     let response = await fetch(`${uri}&beginIndex=${index}&endIndex=${index+dataSize}`);
+        //     let body = await response.json();
             
-            if(!this.verifyBody(body)) break;
+        //     if(!this.verifyBody(body)) break;
 
-            var { matches } = body;
-            console.log(body);
-            index = body.endIndex;
-            totalGames = body.totalGames;
+        //     var { matches } = body;
+        //     console.log(body);
+        //     index = body.endIndex;
+        //     totalGames = body.totalGames;
 
-            // totalGames = body.totalGames >= dataSize ? dataSize : body.totalGames;
-            console.log(`${Math.floor((index / totalGames) * 100)}% / 100%`);
-            matches.map(({ gameId, champion }) => data.push({ gameId, champion }));
-        } while (index !== totalGames);
+        //     // totalGames = body.totalGames >= dataSize ? dataSize : body.totalGames;
+        //     console.log(`${Math.floor((index / totalGames) * 100)}% / 100%`);
+        //     matches.map(({ gameId, champion }) => data.push({ gameId, champion }));
+        // } while (index !== totalGames);
         
         return data;
     }
